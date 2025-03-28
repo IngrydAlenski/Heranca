29dddf8f7e9c199e84e41558f95b002ae4fcdf12
fic <fic@NOTE25-S28>
2025-03-27 21:57:29 -0300

    Commit inicial

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// Henranca - Uma classe pode pegar todos os atributos e metodos de outra classe 
+
+
+using Heranca;
+
+CarroEletrico carroE = new  CarroEletrico();
+
+carroE.Marca = "Tesla";
+carroE.Bateria = 200;
+
+CarroCombustao carroC = new CarroCombustao();
+
+carroC.Marca = "Fiat";
+carroC.Combustivel = "Gasolina";
+
+Carro meuCarro;
+
+Console.WriteLine("Digite E - para Eletrico ou Digite ");
+
+// Pessoa - Nome 
+// PessoaFisica - CPF (classe Filho)
+// PessoaJuridica
