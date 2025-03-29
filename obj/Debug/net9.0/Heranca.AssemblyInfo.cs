7beb04ccf92514504495b18dc7c3bb2f74a8103d
fic <fic@NOTE25-S28>
2025-03-28 21:53:07 -0300

    Aula 28/03

--- obj/Debug/net9.0/Heranca.AssemblyInfo.cs
+++ obj/Debug/net9.0/Heranca.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Heranca")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a06b5cfc9c405e555eac55b965ea8693ec805b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Heranca")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Heranca")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
